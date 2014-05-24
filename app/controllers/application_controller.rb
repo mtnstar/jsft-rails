b9c70ce10007304441f4fc64653a6c58753b0714
pascal simon <ps@s-it.ch>
2014-05-24 14:17:55 +0200

    disable CSRF for alle requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def verified_request?
-    if request.content_type == "application/json"
       true
-    else
-      super()
-    end
   end
 end
